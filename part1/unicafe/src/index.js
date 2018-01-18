25ff2e3f39d09de325bbacdd18e02ce0bb2c5797
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2018-01-18 18:21:28 +0200

    All done up to part 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -17,15 +17,15 @@ class App extends React.Component {
     countReview = () => {
         this.setState({palautteidenLkm: this.state.palautteidenLkm + 1})
     }
-    goodReview = () => {
+    goodReview = () => () => {
         this.countReview()
         this.setState({hyva: this.state.hyva + 1, keskiarvonOsoittaja: this.state.keskiarvonOsoittaja + 1})
     }
-    neutralReview = () => {
+    neutralReview = () => () => {
         this.countReview()
         this.setState({neutraali: this.state.neutraali + 1})
     }
-    badReview = () => {
+    badReview = () => () => {
         this.countReview()
         this.setState({huono: this.state.huono + 1, keskiarvonOsoittaja: this.state.keskiarvonOsoittaja - 1})
     }
@@ -35,34 +35,80 @@ class App extends React.Component {
             <div>
                 <h2>Anna Palautetta</h2>
                 <div>
-                    <button onClick={this.goodReview}>Hyvä</button>
-                    <button onClick={this.neutralReview}>Neutraali</button>
-                    <button onClick={this.badReview}>Huono</button>
+                    <Button handleClick={this.goodReview()} text="Hyvä"/>
+                    <Button handleClick={this.neutralReview()} text="Neutraali"/>
+                    <Button handleClick={this.badReview()} text="Huono"/>
                 </div>
                 <h2>Statistiikka</h2>
 
-                <p>Hyvä: {this.state.hyva}</p>
-                <p>Neutraali: {this.state.neutraali}</p>
-                <p>Huono: {this.state.huono}</p>
-                <PalautteidenKeskiarvo keskiarvonOsoittaja={this.state.keskiarvonOsoittaja}
-                                       palautteidenLkm={this.state.palautteidenLkm}/>
-                <PositiivistenOsuus hyva={this.state.hyva} palautteidenLkm={this.state.palautteidenLkm}/>
+                <Statistics hyva={this.state.hyva}
+                            neutraali={this.state.neutraali}
+                            huono={this.state.huono}
+                            keskiarvonOsoittaja={this.state.keskiarvonOsoittaja}
+                            palautteidenLkm={this.state.palautteidenLkm}/>
             </div>
         )
     }
 }
 
-const PalautteidenKeskiarvo = ({keskiarvonOsoittaja, palautteidenLkm}) => {
-    let ka = keskiarvonOsoittaja / palautteidenLkm;
+const Button = ({handleClick, text}) => {
+    return (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
+}
+
+const Statistics = (props) => {
+    return (
+        <div>
+            <Statistic tyyppi="hyva" arvo={props.hyva}/>
+            <Statistic tyyppi="neutraali" arvo={props.neutraali}/>
+            <Statistic tyyppi="huono" arvo={props.huono}/>
+            <Statistic tyyppi="keskiarvo" arvo={props.keskiarvonOsoittaja} lkm={props.palautteidenLkm}/>
+            <Statistic tyyppi="positiiviset" arvo={props.hyva} lkm={props.palautteidenLkm}/>
+        </div>
+    )
+}
+
+const Statistic = ({tyyppi, arvo, lkm}) => {
+    switch (tyyppi) {
+        case "keskiarvo":
+            return (
+                <PalautteidenKeskiarvo osoittaja={arvo} palautteidenLkm={lkm}/>
+            )
+        case "positiiviset":
+            return (
+                <PositiivistenOsuus hyvia={arvo} palautteidenLkm={lkm}/>
+            )
+        case "hyva":
+            return (
+                <p>Hyvä: {arvo}</p>
+            )
+        case "neutraali":
+            return (
+                <p>Neutraali: {arvo}</p>
+            )
+        case "huono":
+            return (
+                <p>Huono: {arvo}</p>
+            )
+        default: break
+    }
+
+}
+
+const PalautteidenKeskiarvo = ({osoittaja, palautteidenLkm}) => {
+    let ka = osoittaja / palautteidenLkm;
     if (palautteidenLkm === 0) ka = 0
     return (
         <p>Keskiarvo: {ka}</p>
     )
 }
 
-const PositiivistenOsuus = ({hyva, palautteidenLkm}) => {
-    let po = (hyva / palautteidenLkm) * 100;
-    if (hyva === 0) po = 0;
+const PositiivistenOsuus = ({hyvia, palautteidenLkm}) => {
+    let po = (hyvia / palautteidenLkm) * 100;
+    if (hyvia === 0) po = 0;
     return (
         <p>Positiivisia: {po}%</p>
     )
